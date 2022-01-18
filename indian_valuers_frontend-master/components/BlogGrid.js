7856431e1f63a40fbc6b604b3f56b17f3e0b7847
Taran Tuteja <82382628+KingTaran@users.noreply.github.com>
2022-01-18 22:42:01 +0530

    Add files via upload

---
+++ indian_valuers_frontend-master/components/BlogGrid.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Image from "next/image";
+
+const BlogGrid = ({ title, list }) => {
+  return (
+    <div className="blogsContainer">
+      <h3>{title}</h3>
+      <div className="grid">
+        {list?.map((blog, index) => {
+          return (
+            <GridItem blog={blog} key={index} />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+const GridItem = ({blog, index}) => {
+  return (
+    <div className="gridItem small" key={index}>
+      <Image src={blog?.bgLink} alt={blog?.title} />
+      <div className="content">
+        <h6>{blog?.type}</h6>
+        <h5>{blog?.title}</h5>
+        <p>{blog?.time}</p>
+      </div>
+    </div>
+  );
+};
+
+export default BlogGrid;
